c997c622e012a23b12f67bb66b0b80c2d84b5495
JulioAvalos <julioavalos2@msn.com>
2020-07-14 20:19:09 -0600

    passing course id to course page and outputting it there

--- src/containers/Course/Course.js
+++ src/containers/Course/Course.js
@@ -2,23 +2,44 @@ import React, { Component } from 'react';
 
 class Course extends Component {
 
+    state = {
+        loadedCourse: null
+    }
+
     componentDidMount () {
         console.log(this.props);
+        this.loadData();
+    }
+
+    componentDidUpdate () {
+        this.loadData();
     }
 
     loadData () {
         if(this.props.match.params.id) {
-            
+            // console.log('initial -> ', this.props.match.params.id);
+            if(!this.state.loadedCourse) {
+                console.log('changing course 1-> ', this.props.match.params.id);
+                this.setState({loadedCourse: {id: +this.props.match.params.id}});
+            } else if( this.state.loadedCourse.id !== +this.props.match.params.id){
+                // console.log('changing course 2 -> ', this.props.match.params.id);
+                this.setState({loadedCourse: {id: +this.props.match.params.id}});
+                // this.props.history.push('/courses/' + this.props.match.params.id);
+            }
         }
     }
 
     render () {
-        return (
-            <div>
-                <h1>_COURSE_TITLE_</h1>
-                <p>You selected the Course with ID: _ID_</p>
-            </div>
-        );
+        let course = <h1>Please choose a course!</h1>;
+        if(this.state.loadedCourse) {
+            course = (
+                <div>
+                    <h1>_COURSE_TITLE_</h1>
+                    <p>You selected the Course with ID: {this.state.loadedCourse.id}</p>
+                </div>
+            );
+        }
+        return course;
     }
 }
 
