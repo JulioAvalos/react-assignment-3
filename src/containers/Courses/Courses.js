82fda57cbf7832a491416befceec58f41d974f45
JulioAvalos <julioavalos2@msn.com>
2020-07-14 21:07:36 -0600

    loading course page below courses page (as a nested component)

--- src/containers/Courses/Courses.js
+++ src/containers/Courses/Courses.js
@@ -33,7 +33,7 @@ class Courses extends Component {
                         } )
                     }
                 </section>
-                <Route path={this.props.match.url + '/:id'} exact component={Course} />
+                <Route path={this.props.match.url + '/:id'} component={Course} />
             </div>
         );
     }
